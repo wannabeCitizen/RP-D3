6f0b3a09c9108fb061122ac7cc49ebc383b9ad1a
kubalaj <joseph.kubala@colorado.edu>
2014-04-20 13:01:15 -0600

    adding another directory to try to fix premission problems

---
+++ web_site/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask import request
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/hackathon', methods=['POST', 'GET'])
+def hackathon():
+    return render_template('hackathon.html')
+
+@app.route('/journal', methods=['POST', 'GET'])
+def journal():
+    return render_template('journal.html')
+
+@app.route('/results', methods=['POST', 'GET'])
+def results():
+    return render_template('results.html')
+
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    return render_template('aboutus.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
